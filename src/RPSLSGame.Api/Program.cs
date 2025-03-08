9b3d350bf19ff306ddb41cd818527807a4371051
Ana Mitkovic <amitkovic016@gmail.com>
2025-03-08 19:08:46 +0100

    chore: enable Swagger only in non-production environments

--- src/RPSLSGame.Api/Program.cs
+++ src/RPSLSGame.Api/Program.cs
@@ -26,9 +26,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != "Production")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
